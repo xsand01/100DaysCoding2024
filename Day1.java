e900a6dbec50d70e51034176eb0ac49839d1561a
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-02 19:54:38 +0800

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,12 @@
+
+package Day1;
+
+
+public class Day1 {
+       public static void main(String[] args) {
+           String nama = "Kurnia Sandy";
+           System.out.println("Nama = "+ nama);
+     
+               
+    }
+}
