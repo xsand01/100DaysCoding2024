b4e26e8e04d1449f741ccfc7262c63ffbb50ad87
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-04 10:30:57 +0800

    System.out.println

---
+++ Day3.java
@@ -0,0 +1,8 @@
+public class Day3 {
+    public static void main(String[] args) {
+        System.out.println("Nama=Kurnia Sandy");
+        System.out.println("Nim=D0224033");
+        System.out.println("Kelas=D");
+        System.out.println("Alamat=Dusun Kebundalam,Desa Bumiayu,Kec.Wonomulyo");
+    }
+}
