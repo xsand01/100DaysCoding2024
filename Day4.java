839ad597537335187efe382fda5f737c216a9251
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-05 22:08:00 +0800

    Update Day4.java

--- Day4.java
+++ Day4.java
@@ -4,7 +4,7 @@ public class Day4 {
         System.out.print("Nim:D0224033 \n"); 
         System.out.print("Kelas:D \n"); 
         
-        System.out.printf(" Nama:Kurnia Sandy %n Nim:D0224033 %n Kelas:D %n" );
+          System.out.printf("Nama: %s%nUmur: %d tahun%nTinggi Badan: %d cm%nBerat badan: %d kg%n", "Kurnia Sandy", 18, 170, 50);
       
     }
   
