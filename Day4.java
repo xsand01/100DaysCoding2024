96e887f339139a3d3524dfccf234f0cbe5b41b2a
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-05 11:21:45 +0800

    System.out.print dan System.out.printf

---
+++ Day4.java
@@ -0,0 +1,11 @@
+public class Day4 {
+    public static void main(String[] args) {
+        System.out.print("Nama:Kurnia Sandy \n"); 
+        System.out.print("Nim:D0224033 \n"); 
+        System.out.print("Kelas:D \n"); 
+        
+        System.out.printf(" Nama:Kurnia Sandy %n Nim:D0224033 %n Kelas:D %n" );
+      
+    }
+  
+}
