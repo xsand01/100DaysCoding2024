93198ef0bc1a3c893a212dca077369026b757c28
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-06 21:16:03 +0800

    Membuat Komentar

---
+++ Day5.java
@@ -0,0 +1,17 @@
+public class Day5 {
+    public static void main(String[] args) {
+        System.out.println("Halo Saya Kurnia Sandy");
+        
+        // di gunakan untuk komentar satu baris
+        
+        /*
+        di gunakan untuk komentar lebih dari satu baris
+        contohnya seperti komentar ini yang bisa lebih dari dua baris
+        */
+        
+        /**
+         *komentar ini adalah komentar khusus yang berfungsi untuk menjelaskan kode
+         */
+        
+       // Semangat Masih ada 95 hari lagi hehe...
+    
