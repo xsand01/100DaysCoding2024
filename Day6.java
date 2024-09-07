aa5a738a03c7193c552125221f4d206b0bb9fa60
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-07 21:13:08 +0800

    Variabel ; Deklarasi

---
+++ Day6.java
@@ -0,0 +1,16 @@
+public class Day6 {
+    public static void main(String[] args) {
+     //Deklarasi   
+   
+    String nama = "Kurnia Sandy";    //String di gunakan untuk menampilkan teks pada variabel
+    int umur = 18;                  // int di gunakan untuk menampilkan bilangan bulat
+    float beratbadan = 45.0f;      // nah float ini di gunakan untuk angka yang berkoma
+    double tinggibadan = 170.0;   // fungsinya sama aja dengan float,cuman ini bisa menampilkan lebih banyak angka di belakang koma
+    
+    //Variabel
+        System.out.println( "Nama : " +nama);
+        System.out.println( "Umur : " +umur);
+        System.out.println( "Tinggi Badan : " +tinggibadan +" cm");
+        System.out.println( "Berat Badan : " +beratbadan +" kg");    
+    }
+}
