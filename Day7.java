8274519d080dfb81342b0c97b904ad9d77f41fae
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-08 09:38:12 +0800

    Variabel : Inisialisasi

--- Day7.java
+++ Day7.java
@@ -1,15 +1,14 @@
-
 public class Day7 {
     
     public static void main(String[] args) {
      //Variabel Inisialisasi  
    
-    String nama = "Kurnia Sandy";    //Inisialisasi variabel String dengan kata Kurnia Sandy
-    int umur = 18;                  // Inisialisasi variabel integer dengan nilai 25
-    float beratbadan = 45.0f;      // Inisialisasi variabel floating dengan angka 45.0
-    double tinggibadan = 170.0;   // Inisialisasi variabel doubel dengan angka 170.0
+    String nama = "Kurnia Sandy";    //Variabel nama di inisialisasikan dengan string Kurnia sandy.
+    int umur = 18;                  // Variabel umur di inisialisasikan dengan nilai integer 18
+    float beratbadan = 45.0f;      // Variabel berat badan di inisialisasikan dengan angka 45.0
+    double tinggibadan = 170.0;   // Variabel berat badan di inisialisasikan dengan angka 170.0
     
-    //Variabel
+   
         System.out.println( "Nama : " +nama);
         System.out.println( "Umur : " +umur +" Tahun");
         System.out.println( "Tinggi Badan : " +tinggibadan +" cm");
