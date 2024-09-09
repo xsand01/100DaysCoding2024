005c5f9c993d3424bc71a7322a3f4be9b8a86ca5
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-09 21:38:48 +0800

    Variabel : Update

---
+++ Day8.java
@@ -0,0 +1,15 @@
+// Day 8 = Variabel Update
+
+         // >>Variabel Awal<<
+public class Day8 {
+    public static void main(String[] args) {
+        int Umur = 18;
+        System.out.println("Umur : " + Umur + "Tahun");
+        
+        //>>Variabel  After Update<<
+        Umur =  20;
+        System.out.println("Umur : " + Umur + " Tahun");
+       
+    }
+  
+}
