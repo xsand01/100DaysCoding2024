341845be3e96c953b6624ae7cf91cbe29533cbf5
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-11 12:03:51 +0800

    Tipe Data.java

---
+++ Day10.java
@@ -0,0 +1,11 @@
+public class Day10 {
+    public static void main(String[] args) {
+        //Day10
+        // Tipe Data Byte
+        
+     byte TipeDataByte =127; // ukuran 1 byte(8bit) dengan rentang nilai -128 sampai 127
+        System.out.println("Angka Tipe Data Byte :" +  TipeDataByte);
+     
+    }
+  
+}
