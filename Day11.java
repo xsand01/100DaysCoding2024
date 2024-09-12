49ccf23f0291448d978e71b92b29bbc29f735886
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-12 21:46:05 +0800

    tipe data short

---
+++ Day11.java
@@ -0,0 +1,14 @@
+public class Day11 {
+ public static void main(String[]Args) {
+     
+ //Tipe data short memiliki range angka dari -32768    
+ short Minimum_Angka = -32768;
+ 
+ // Hingga maksimal sampai 32767
+ short Maksimal_Angka = 32767;
+ 
+     System.out.println("Minimum :" + Minimum_Angka);
+     System.out.println("Minimum :" + Maksimal_Angka);
+     
+ }   
+}
