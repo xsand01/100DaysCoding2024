bf09534e0294ddcc2387d3eeb875d3721cadff50
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-13 17:40:11 +0800

    Tipe Data int.java

---
+++ Day_12.java
@@ -0,0 +1,16 @@
+package Coding100Day;
+
+public class Day_12 {
+
+    public static void main(String[] Args) {
+        // Day 12
+        // Tipe Data int
+        // int adalah tipe data primitif yang memiliki range nilai dari -2147483648 sampai 2147483647
+        
+        int Minimal = -2147483648;
+        int Maksimal = 2147483647;
+        System.out.println("Range Nilai Int ");
+        System.out.println("Nilai Minimal = " + Minimal);
+        System.out.println("Nilai Maksimal = " + Maksimal);
+    }
+}
