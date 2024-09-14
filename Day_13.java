f4ed667b23cb524d53fc96194da6672b9ec412e1
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-14 10:08:39 +0800

    Evaluasi.java

---
+++ Day_13.java
@@ -0,0 +1,26 @@
+package Evaluasi1;
+
+public class Evaluasi {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        short c = 30;
+        short d = 40;
+
+        byte e = 50;
+        byte f = 60;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+        System.out.println("f = " + f);
+
+        System.out.printf("\na= %d%nb=%d%nc=%d%nd=%d%ne=%d%nf=%d%n", a, b, c, d, e, f);
+
+    }
+
+}
