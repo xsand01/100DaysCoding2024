6f2b30c8a9e90ab183ac397adece378aa0eb4c90
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-15 23:12:47 +0800

    Day_14Tipe_Data_Long.java

--- Day_14.java
+++ Day_14.java
@@ -3,10 +3,18 @@ public class Day_14 {
  public static void main(String[]args){
      
      //Tipe Data -> long -> Bilangan Bulat -> 64bit
+     /*Di sini saya mendeklarasikan dengan  nama variable min dengan tipe data long dan
+     saya inisialisasikan dengan nilai minimal yang bisa di isikan pada tipe data long tersebut,begitu
+     juga pada variabel kedua,saya mendeklarasikan dengan nama variabel max dengan tipe data long dan saya inisialisasikan 
+     dengan nilai maksimal yang dapat di simpan pada tipe data long.
+     */
      long min = -9223372036854775808L ;
      long max =  9223372036854775807L ;
      
+     // *Kemudian di sini saya sout untuk dapat menampilkan variabel di atas pada output
      System.out.println("Nilai Minimal Long  = " + min);
      System.out.println("Nilai Maksimal Long = " + max);
+     
  }   
 }
+ 
