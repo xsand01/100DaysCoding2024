e13fe2fe4252425b730042763109e8ca0f4109de
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-15 21:44:19 +0800

    Day_14TipeDataLong.java
    Tipe data long

---
+++ Day_14.java
@@ -0,0 +1,12 @@
+package Coding100Day;
+public class Day_14 {
+ public static void main(String[]args){
+     
+     //Tipe Data -> long -> Bilangan Bulat -> 64bit
+     long min = -9223372036854775808L ;
+     long max =  9223372036854775807L ;
+     
+     System.out.println("Nilai Minimal Long  = " + min);
+     System.out.println("Nilai Maksimal Long = " + max);
+ }   
+}
