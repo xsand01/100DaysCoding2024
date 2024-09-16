d3f0691ce375cce431af6c079b2ad36ad0b1c80e
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-16 20:24:31 +0800

    Tipe Data double.java
    Tipe Data double

---
+++ Day_15.java
@@ -0,0 +1,16 @@
+package Coding100Day;
+
+public class Day_15 {
+
+    public static void main(String[] args) {
+        //Tipe data double -> double -> bilangan berkoma ->64bit
+        //double adalah tipe data yang hanya bisa di isi oleh bilangan desimal atau bilangan berkoma
+        //berikut adalah contohnya 
+
+        double tb = 170.0;
+        double bb = 65.5;
+
+        System.out.println("Tinggi Badan =" + tb);
+        System.out.println("Berat Badan =" + bb);
+    }
+}
