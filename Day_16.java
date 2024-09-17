e514536f5e59b1525e70c8ea81a905214030b2f1
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-17 13:48:47 +0800

    Tipe Data float.java
    Day 16 Tipe Data float

---
+++ Day_16.java
@@ -0,0 +1,21 @@
+package Coding100Day;
+
+public class Day_16 {
+
+    public static void main(String[] args) {
+        // Day 16
+        // Tipe Data Floating -> float -> Bilangan Desimal atau Berkoma -> 32bit
+        /*Tipe data float adalah tipe data yang di gunakan untuk menaruh bilangan desimal atau berkoma.
+          Tipe data ini sama dengan tipe data double,namun bedanya tipe data ini memiliki kapasitas 32bit dan 
+          jika kita memakai tipe data float ini pada akhir bilangan harus di beri huruf f sebagai pertanda 
+          bahwa kita memakai tipe data float.
+         */
+
+        float TB = 170.0f;
+        float BB = 65.5f;
+
+        System.out.println("Berat Badan  = " + TB + " kg");
+        System.out.println("Tinggi Badan = " + BB + " cm");
+
+    }
+}
