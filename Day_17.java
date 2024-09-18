ddc150281c24c60d3ffec6d9baea1b18c6d4e447
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-18 19:22:12 +0800

    TipeData_char.java
    tipe data char

---
+++ Day_17.java
@@ -0,0 +1,15 @@
+package Coding100Day;
+
+public class Day_17 {
+
+    public static void main(String[] args) {
+
+        // Tipe data char -> char -> primitif -> 1 karakter
+        //tipe data char adalah tipe data primitif yang hanya bisa menampung satu karakter seperti huruf maupun angka
+        char inisial = 'S';
+        char GolonganDarah = 'O';
+
+        System.out.println(" Inisial = " + inisial);
+        System.out.println(" Golongan Darah = " + GolonganDarah);
+    }
+}
