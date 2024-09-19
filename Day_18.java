9af89d0c33c07af9fd793f35fb609ac0250689e5
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-19 11:23:25 +0800

    TipeDataBoolean.java
    Tipe data boolean

---
+++ Day_18.java
@@ -0,0 +1,15 @@
+package Coding100Day;
+
+public class Day_18 {
+
+    public static void main(String[] args) {
+        // tipe Data boolean -> boolean-> true & false
+        /*Tipe data boolean adalah tipe data yang hanya mempunyai dua karakter yaitu true & false*/
+        boolean kesehatan1 = true;
+        boolean kesehatan2 = false;
+        System.out.println("Kondisi Kesehatan Kurnia Sandy");
+        System.out.println("===============================");
+        System.out.println("Sehat = " + kesehatan1);
+        System.out.println("Tidak Sehat = " + kesehatan2);
+    }
+}
