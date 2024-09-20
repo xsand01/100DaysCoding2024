a50d112e753efc5b4821e374e9a19e06fb2642bf
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-20 13:58:36 +0800

    TipeDataString.java
    Tipe Data String

---
+++ Day_19.java
@@ -0,0 +1,13 @@
+package Coding100Day;
+
+public class Day_19 {
+
+    public static void main(String[] args) {
+        // Tipe data String->String->menampilkan karakter
+        // String adalah tipe data referenc yang digunakan untuk menyimpan karakter huruf ataupun angka
+        // NOTE = kalo untuk matematis lebih baik menggunkan int atau long dll. karena string membaca angka sebagai karakter
+
+        String nama = "Kurnia Sandy";
+        System.out.println("NAMA = " + nama);
+    }
+}
