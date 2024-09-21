9c1474b94859fa6ecd2a4da4f3d02a14e5917bc0
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-21 22:21:13 +0800

    TipeDataByte.java
    tipe data reference Byte

---
+++ Day_20.java
@@ -0,0 +1,14 @@
+package Coding100Day;
+
+public class Day_20 {
+
+    public static void main(String[] args) {
+// tipe data Byte -> Reference 
+
+        Byte angkaObjek = 25; // mendeklarasikan objek Byte
+        
+        System.out.println("Nilai Byte = " + angkaObjek); // akan mencetak 25
+
+    }
+
+}
