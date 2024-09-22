b22c3a35f893a4bb5695c50c76596e3285215dcf
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-22 23:05:24 +0800

    TipeDataStringReferance.java
    membuat tipe data referance String

---
+++ Day_21.java
@@ -0,0 +1,12 @@
+package Coding100Day;
+
+public class Day_21 {
+// tipe data referance String
+    public static void main(String[] args) {
+        
+        Short angka = 1000;
+        System.out.println("nilai Short = " + angka);
+          
+    }
+
+}
