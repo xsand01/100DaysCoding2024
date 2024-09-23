27e6f392ed5f7c721589c630f5c6b85a3f312619
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-23 19:12:44 +0800

    TipeDataInteger.java
    Tipe data referance Integer

---
+++ Day_22.java
@@ -0,0 +1,11 @@
+package Coding100Day;
+
+public class Day_22 {
+
+    public static void main(String[] args) {
+        //Tipe Data Integer adalah tipe data pembungkus
+        //Tipe Data Integer juga bisa menyimpan nilai null
+        Integer angka = null;
+        System.out.println("Nilai Integer = " + angka);
+    }
+}
