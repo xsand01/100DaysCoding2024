0e503b6fc4a1f24d104660c316ef31dd1393c709
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-24 16:25:49 +0800

    TipeDataLong.java
    Tipe data referensi Long

---
+++ Day_23.java
@@ -0,0 +1,10 @@
+package Coding100Day;
+
+public class Day_23 {
+
+    public static void main(String[] args) {
+        long angka = 2000000L;
+        Long kelasPembungkus = angka;
+        System.out.println("Angka Long = " + kelasPembungkus);
+    }
+}
