c496773b7736b31052c09257cdde4cf51a34283f
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-25 19:36:37 +0800

    TipeDataFloat.java
    Tipe data reference Float

---
+++ Day_24.java
@@ -0,0 +1,10 @@
+package Coding100Day;
+
+public class Day_24 {
+
+    public static void main(String[] args) {
+        float TipeDataPrimitif = 20.9f;
+        Float KelasPembungkus = TipeDataPrimitif;
+        System.out.println("Nilai Float = " + KelasPembungkus);
+    }
+}
