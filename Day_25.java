72c993f16566c57b61f6bf01a92ec6a0bf93af22
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-26 19:37:58 +0800

    TipeDataReferensiDouble.java
    tipe data referensi Double

---
+++ Day_25.java
@@ -0,0 +1,10 @@
+package Coding100Day;
+
+public class Day_25 {
+
+    public static void main(String[] args) {
+        double angka1 = 65.5;
+        Double kelasPembungkus = angka1;
+        System.out.println("Nilai Double = " + angka1);
+    }
+}
