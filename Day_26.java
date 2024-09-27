120e9da6058f4a4c43806b94c24d9a73f6ca6e21
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-27 18:00:14 +0800

    TipeDataReferensiCharacter.java
    Tipe Data Referensi Character

---
+++ Day_26.java
@@ -0,0 +1,13 @@
+package Coding100Day;
+
+public class Day_26 {
+
+    public static void main(String[] args) {
+        //Tipe data referensi Char
+        char inisial = 'S';
+        Character KelasPembungkus = inisial;
+
+        System.out.println("Nama saya adalah = Sandy ");
+        System.out.println("Inisial Saya adalah = " + KelasPembungkus);
+    }
+}
