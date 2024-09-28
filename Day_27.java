9f57432ab5953e47a35afd64409c61522d7adbf3
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-28 23:31:18 +0800

    Evaluasi.java

---
+++ Day_27.java
@@ -0,0 +1,13 @@
+package Coding100Day;
+
+public class Day_27Evaluasi {
+
+    public static void main(String[] args) {
+        System.out.println("Selamat Datang");
+        final int jumlahFilm = 50;
+        String judulFilm = "Ironman";
+        Short tahunRilis = 2010;
+        judulFilm = "From";
+        System.out.printf("Film berjudul %s di rilis pada tahun %d dan terdapat %d film pada katalog saat ini %n", judulFilm, tahunRilis, jumlahFilm);
+    }
+}
