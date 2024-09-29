a20d8be524186cc3c884b26fedf15c3803ab9897
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-29 23:00:28 +0800

    TipeDataReferensiBoolean.java
    tipe data referensi Boolean

---
+++ Day_28.java
@@ -0,0 +1,13 @@
+package Coding100Day;
+
+public class Day_28 {
+
+    public static void main(String[] args) {
+        // Tipe Data Referensi Boolean 
+        Boolean a = true;
+        Boolean b = false;
+        System.out.println("Apakah sandy sudah kerja tugas = " + b);
+        System.out.println("Apakah sandy belum kerja tugas = " + a);
+
+    }
+}
