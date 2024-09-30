c0a5f8a99147fa583bd81c0a7ce29dcd1eb86215
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-09-30 22:06:49 +0800

    BigInteger.java
    Tipe data Referensi BigInteger

---
+++ Day_29.java
@@ -0,0 +1,14 @@
+package Coding100Day;
+
+import java.math.BigInteger;
+
+public class Day_29 {
+
+    public static void main(String[] args) {
+        BigInteger a = new BigInteger("123456789012345678901234567890");
+        BigInteger b = new BigInteger("123456789123456789123456789");
+        System.out.println("Nilai Big Integer 1 = " + a);
+        System.out.println("Nilai Big Integer 1 = " + b);
+    }
+
+}
