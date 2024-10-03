178b5ebf6d60ed70a9de69d884fa996bc47eb88a
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-10-03 20:52:53 +0800

    konversiPrimitf>String.java
    Konversi Tipe Data Primitif ke String

--- Day_32.java
+++ Day_32.java
@@ -8,23 +8,23 @@ public class Day_32 {
         //Metode konversi 1 (Cara paling umum dan aman)
         int angka = 100;
         String konversi = String.valueOf(angka);
-        System.out.println("Metode 1 = " + angka);
-        
+        System.out.println("Metode 1 = " + konversi);
+
         //Metode konversi 2 (metode serupa untuk tipe lain)
         int angka1 = 101;
-        String koversi1 = Integer.toString(angka1);
-        System.out.println("Metode 2 = " + angka1);
-        
+        String konversi2 = Integer.toString(angka1);
+        System.out.println("Metode 2 = " + konversi2);
+
         //Metode konversi 3 
         int angka2 = 102;
         String konversi3 = angka2 + "";
         System.out.println("Metode 3 = " + konversi3);
-        
+
         //Metode konversi 4
         int angka3 = 103;
         String konversi4 = String.format("%d", angka3);
         System.out.println("Metode 4 = " + konversi4);
-        
+
         //Metode konversi 5 (lebih umum di gunakan ketika ingin menggabungkan beberapa String atau tipe data
         int angka4 = 104;
         StringBuilder konversi5 = new StringBuilder();
