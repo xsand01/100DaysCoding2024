0f4c2ab6845b4713e7e94e0a8abbf8b6f5af58b7
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-10-04 23:27:44 +0800

    Casting.java

--- Day_33.java
+++ Day_33.java
@@ -3,17 +3,16 @@ package Coding100Day;
 public class Day_33 {
 
     public static void main(String[] args) {
+
+        // implicit casting (widening)
+        int angka4 = 1270; //konversi otomatis
+        double angka5 = angka4;
+        System.out.println("Nilai sebelum di casting = " + angka4);
+        System.out.println("Nilai sesudah di casting = " + angka5);
         // explicit casting (narrowing)
         double angka1 = 20.11;
         int angka2 = (int) angka1;//konversi manual
         System.out.println("Nilai sebelum di casting = " + angka1);
         System.out.println("Nilai sesudah di casting = " + angka2);
-
-        // implicit casting (widening)
-        int angka4 = 1270; //konversi otomatis
-        double angka5 = (double) angka4;
-        System.out.println("Nilai sebelum di casting = " + angka4);
-        System.out.println("Nilai sesudah di casting = " + angka5);
-
     }
 }
