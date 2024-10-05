02f727c5e6726c63058b38434210ef1672164ba0
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-10-05 15:33:47 +0800

    InputDariKeyboard.java
    Scanner

---
+++ Day_34..java
@@ -0,0 +1,24 @@
+package Coding100Day;
+
+import java.util.Scanner;
+
+public class Day_34 {
+
+    public static void main(String[] args) {
+        //Input dari keyboard (Scanner)
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukan Nama = ");
+        String nama = input.nextLine();
+        System.out.print("Masukan Umur = ");
+        int umur = input.nextInt();
+        System.out.print("Masukan Tinggi Badan = ");
+        Double tb = input.nextDouble();
+        System.out.print("Masukan Berat Badan = ");
+        double bb = input.nextDouble();
+        System.out.println("===================================================");
+        System.out.println("Nama " + nama + " berumur " + 18 + " Tahun memiliki tinggi badan " + tb + " cm " + " dan memiliki Berat Badan " + bb + " kg ");
+
+    }
+}
+  
