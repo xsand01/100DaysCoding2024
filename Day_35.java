9726d4c53e040acba2b801a783d532b0970240c9
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-10-06 23:16:27 +0800

    OperatorAritmatikaPenjumlahan_Pengurangan.java

---
+++ Day_35.java
@@ -0,0 +1,17 @@
+package Coding100Day;
+
+public class Day_35 {
+
+    public static void main(String[] args) {
+
+        //Operator aritmatika penjumlahan dan pengurangan
+        int a = 20;
+        int b = 10;
+        int penjumlahan = a + b;
+        int pengurangan = a - b;
+        System.out.println(" Hasil dari penjumlahan = " + penjumlahan);
+        System.out.println(" Hasil dari pengurangan = " + pengurangan);
+
+    }
+
+}
