f955466956bd072a63b9b23c06d485a5ee2c62eb
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-10-07 19:41:05 +0800

    OperatorAritmatika.java
    Perkalian , pembagian , Modulus

---
+++ Day_36.java
@@ -0,0 +1,14 @@
+package Coding100Day;
+
+public class Day_36 {
+
+    public static void main(String[] args) {
+        //Operator aritmatka perkalian,pembagian,modulus
+        int a = 100;
+        int b = 25;
+
+        System.out.printf(" Hasil Perkalian = %d%n Hasil dari Pembagian = %d%n Hasil dari Modulus = %d%n ", a * b, a / b, a % b);
+
+    }
+
+}
