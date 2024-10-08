c1eab6c25784dd1793ef12f2a9692283153d529d
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-10-08 19:58:13 +0800

    OperatorPenugasan.java
     membuat operator penugasan Pengisian Nilai, Pengisian dan Penambahan, Pengisian dan Pengurangan

---
+++ Day_37.java
@@ -0,0 +1,25 @@
+package Coding100Day;
+
+public class Day_37 {
+
+    public static void main(String[] args) {
+        //Day 37 membuat operator penugasan Pengisian Nilai, Pengisian dan Penambahan, Pengisian dan Pengurangan 
+
+        //Pengisian
+        int a = 100;
+        System.out.println("Nilai a = " + a);
+
+        // Pengisian dan Penjumlahan
+        a += 25;
+        System.out.println("Nilai a setelah di jumlah = " + a);
+
+        //Pengisian
+        int b = 100;
+        System.out.println("Nilai b = " + b);
+
+        //Pengisian dan Pengurangan
+        b -= 25;
+        System.out.println("Nilai b setelah di kurang = " + b);
+
+    }
+}
