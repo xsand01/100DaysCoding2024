132ba86ec8cf34261ef6533cb9e683e6c3a372fc
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-10-09 22:08:37 +0800

    OperatorPenugasan.java
    Operator Penugasan : Pengisian dan Perkalian, Pengisian dan Pembagian

---
+++ Day_38.java
@@ -0,0 +1,32 @@
+package Coding100Day;
+
+import java.util.Scanner;
+
+public class Day_38 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("================================");
+        System.out.println("Perkalian");
+        System.out.println("================================");
+        System.out.print("Masukan angka a = ");
+        int a = input.nextInt();
+        System.out.print("Masukan angka b = ");
+        int b = input.nextInt();
+        a *= b;
+        System.out.println("Hasil perkalian = " + a);
+
+        System.out.println("================================");
+        System.out.println("Pembagian");
+        System.out.println("================================");
+        System.out.print("Masukan angka a = ");
+        int angka1 = input.nextInt();
+        System.out.print("Masukan angka b = ");
+        int angka2 = input.nextInt();
+        angka1 /= angka2;
+        System.out.println("Hasil Pembagian = ");
+
+    }
+}
+
