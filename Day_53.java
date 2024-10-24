bd53a251de1f19e937780397806d50ed3bbed880
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-10-24 21:37:31 +0800

    for looping.java

---
+++ Day_53.java
@@ -0,0 +1,12 @@
+package Coding100Day;
+
+public class Day_53 {
+
+    public static void main(String[] args) {
+        for (int a = 1; a <= 10;) {
+            System.out.println("Kurnia Sandy");
+        }
+
+    }
+
+}
