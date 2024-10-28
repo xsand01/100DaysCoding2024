463a03afebbdf0a91c412479c232a86e6a1bf16f
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-10-28 23:35:48 +0800

    Looping : while Increment .java
    Looping : while Increment

---
+++ Day_57.java
@@ -0,0 +1,16 @@
+package Coding100Day;
+
+public class Day_57 {
+    //Looping : while Increment 
+
+    public static void main(String[] args) {
+        int a = 1;
+        while (a <= 10) {
+            System.out.println(a);
+            a++;
+
+        }
+
+    }
+
+}
