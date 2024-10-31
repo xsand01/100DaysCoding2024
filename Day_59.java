6f7832fc13c4ad8ce3ff8067189c3a8ff84ca138
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-10-31 21:40:58 +0800

    Looping_For_Each.java
    Looping : For Each

---
+++ Day_59.java
@@ -0,0 +1,12 @@
+package Coding100Day;
+
+public class Day_59 {
+
+    public static void main(String[] s) {
+        String[] namaTeman = {"Alan", "Gibran", "Sabri"};
+
+        for (String n : namaTeman) {
+            System.out.println("Nama teman saya : " + n);
+        }
+    }
+}
