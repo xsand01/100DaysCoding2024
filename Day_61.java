2982ca8154bd68b33857fc6157d34613538eeaf3
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-11-01 22:40:33 +0800

    Infinite Loop .java
    Infinite Loop

---
+++ Day_61.java
@@ -0,0 +1,16 @@
+package Coding100Day;
+
+public class Day_61 {
+
+    public static void main(String[] args) {
+//1. infinite loop pada looping for
+        for (int a = 0; a<= 10;) {
+            System.out.println("Infinite Loop/Tanpa batas");
+
+        }
+//2.infinite loop pada looping while
+        while (true) {
+            System.out.println("aku cinta kamu tanpa batas");
+        }
+    }
+}
