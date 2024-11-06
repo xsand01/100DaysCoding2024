c4e37a4af3c3db2cdefd34015a21109e1c8509dd
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-11-07 00:48:12 +0800

    PolaPersegi&PersegiPanjang.java

--- Day_66.java
+++ Day_66.java
@@ -1,30 +1,39 @@
 package Coding100Day;
 
+import java.util.Scanner;
+
 public class Day_66 {
 
     public static void main(String[] args) {
-        System.out.println("=====PERSEGI======");
+        Scanner in = new Scanner(System.in);
+        System.out.println("PERSEGI(inputkan nilai yang sama pada baris dengan kolom )");
+        System.out.print("Masukan Baris = ");
+        int baris = in.nextInt();
+        System.out.print("Masukan Kolom = ");
+        int kolom = in.nextInt();
         System.out.println(" ");
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 6; j++) {
+        for (int i = 1; i <= baris; i++) {
+            for (int j = 1; j <= kolom; j++) {
 
                 System.out.print("* ");
             }
             System.out.println(" ");
         }
         System.out.println(" ");
-        System.out.println("======PERSEGI PANJANG======");
+        System.out.println("PERSEGI PANJANG (inputkan nilai baris lebih kecil dari pada nilai kolom)");
+        System.out.print("Masukan Baris = ");
+        int baris1 = in.nextInt();
+        System.out.print("Masukan Kolom = ");
+        int kolom1 = in.nextInt();
         System.out.println(" ");
-        for (int k = 1; k <= 5; k++) {
-            for (int p = 0; p <= 10; p++) {
+        for (int k = 1; k <= baris1; k++) {
+            for (int p = 1; p <= kolom1; p++) {
                 System.out.print("* ");
 
             }
             System.out.println(" ");
         }
-     
-            
-        }
+
     }
 
-
+}
