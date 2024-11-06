a458b015e99906ab5537f632ae13cc65920570d2
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-11-07 00:17:17 +0800

    PolaPersegi/PersegiPanjang.java

---
+++ Day_66.java
@@ -0,0 +1,30 @@
+package Coding100Day;
+
+public class Day_66 {
+
+    public static void main(String[] args) {
+        System.out.println("=====PERSEGI======");
+        System.out.println(" ");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 6; j++) {
+
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+        System.out.println(" ");
+        System.out.println("======PERSEGI PANJANG======");
+        System.out.println(" ");
+        for (int k = 1; k <= 5; k++) {
+            for (int p = 0; p <= 10; p++) {
+                System.out.print("* ");
+
+            }
+            System.out.println(" ");
+        }
+     
+            
+        }
+    }
+
+
