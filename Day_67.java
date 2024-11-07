d26fe2133a2f7b4aaa7ce4384cd77deb97a25f1d
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-11-07 19:55:29 +0800

    MethodVoid.java

---
+++ Day_67.java
@@ -0,0 +1,16 @@
+package Coding100Day;
+
+public class Day_67 {
+
+    public static void saya() {
+        String nama = "Kurnia Sandy";
+        int umur = 18;
+        System.out.println("Nama = " + nama + "\n" + "Umur = " + umur);
+
+    }
+
+    public static void main(String[] args) {
+        saya();
+
+    }
+}
