498de47e5b869897328240162b8b011c2e1048e9
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-11-09 23:47:14 +0800

    Method Parameter .java

---
+++ Day_69.java
@@ -0,0 +1,29 @@
+package Coding100Day;
+
+import java.util.Scanner;
+
+public class Day_69 {
+
+    public static int perkalian(int a, int b) {
+        return a * b;
+
+    }
+
+    public static int pembagian(int x, int y) {
+        return x / y;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukan nilai a = ");
+        int a = in.nextInt();
+        System.out.print("Masukan nilai b = ");
+        int b = in.nextInt();
+        int hasilKali = perkalian(a, b);
+        int hasilBagi = pembagian(a, b);
+        System.out.println("====================================================");
+
+        System.out.println("Hasil penjumlahan = " + hasilKali + " \n" + "Hasil Pengurangan = " + hasilBagi); // Output: Hasil penjumlahan: 8
+    }
+
+}
