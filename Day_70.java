b55f61f7ae3912aade229b76a87ac069fc4c1c26
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-11-11 23:43:23 +0800

    Method parameter.java

--- Day_70.java
+++ Day_70.java
@@ -2,32 +2,29 @@ package Coding100Day;
 
 import java.util.Scanner;
 
-public class Day_70 {
-//Method Static 
+public class Day_69 {
+//Method Parameter
 
-    /*  Static method pada Java adalah metode yang dideklarasikan dengan kata kunci static, 
-    sehingga bisa dipanggil tanpa harus membuat objek dari kelas tersebut. 
-    Metode ini hanya dapat mengakses variabel dan metode lain yang juga bersifat static dan sering digunakan 
-    untuk fungsi yang tidak bergantung pada data spesifik suatu objek, seperti fungsi utility. 
-    Contoh umum static method adalah metode main dalam Java, yang memungkinkan
-    program dijalankan langsung oleh Java Virtual Machine (JVM) tanpa membuat objek.
-     */
-    public static int modulus(int a, int b) {
-        return a % b;
+    public static int perkalian(int a, int b) {
+        return a * b;
+
+    }
+
+    public static int pembagian(int x, int y) {
+        return x / y;
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Masukan angka untuk menentukan sisa bagi nya");
-        System.out.println("=============================================");
-        System.out.print("Masukan angka pertama = ");
+        System.out.print("Masukan nilai a = ");
         int a = in.nextInt();
-        System.out.print("Masukan angka kedua = ");
+        System.out.print("Masukan nilai b = ");
         int b = in.nextInt();
+        int hasilKali = perkalian(a, b);
+        int hasilBagi = pembagian(a, b);
+        System.out.println("====================================================");
 
-        int hasil = modulus(a, b);
-        System.out.println("==============================================");
-        System.out.println("Sisa bagi dari " + a + " dan " + b + " adalah = " + hasil);
+        System.out.println("Hasil penjumlahan = " + hasilKali + " \n" + "Hasil Pengurangan = " + hasilBagi); // Output: Hasil penjumlahan: 8
     }
 
 }
