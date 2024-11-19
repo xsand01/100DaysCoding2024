fdbe2aa265cc7864fb736092724af9325167dabe
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-11-19 23:59:47 +0800

    toString.java

--- Day_79.java
+++ Day_79.java
@@ -2,27 +2,9 @@ package Coding100Day;
 
 public class Day_79 {
 
-    String nama;   //untuk nama mobil
-    String warna;  // untuk warna mobil
-
-    public Day_79(String nama, String warna) {
-        this.nama = nama;
-        this.warna = warna;
-    }
-
-    // Override metode toString
-    @Override
-    public String toString() {
-        return "Mobil[ nama = " + nama + ", warna = " + warna + " ]";
-    }
-
     public static void main(String[] args) {
-        // Membuat objek mobil
-        Day_79 mobil1 = new Day_79(" Toyota ", " Merah ");
-        Day_79 mobil2 = new Day_79(" Honda ", " Biru ");
+        String a = "Nama : Kurnia Sandy\nUmur : 18 Tahun\nStatus : Masih tanda tanya\n";
+        System.out.println(a.toString());
 
-        // Mencetak objek mobil
-        System.out.println(mobil1);
-        System.out.println(mobil2);
     }
 }
