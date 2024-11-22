00c0a4e2d3c558cb717037471cee8a7f6187d8b2
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-11-22 22:52:36 +0800

    Method ceil(),floor()_dan_round().java

---
+++ Day_82.java
@@ -0,0 +1,37 @@
+package Coding100Day;
+
+import java.util.Scanner;
+
+public class Day_82 {    
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        /*Math.ceil()
+Fungsi: Membulatkan angka ke atas ke bilangan bulat terdekat.
+Tipe Kembalian: double.*/
+        System.out.print("Masukan bilangan desimal = ");
+        double a = in.nextDouble();
+        System.out.println("Hasil dari Math.cheil  = " + Math.ceil(a));
+
+        /*Math.floor()
+Fungsi: Membulatkan angka ke bawah ke bilangan bulat terdekat.
+Tipe Kembalian: double.
+         */
+        System.out.print("Masukan bilangan desimal = ");
+        double b = in.nextDouble();
+        System.out.println("Hasil dari Math.flor = " + Math.floor(a));
+
+        /*Math.round()
+Fungsi: Membulatkan angka ke bilangan bulat terdekat (aturan pembulatan biasa).
+Tipe Kembalian:
+Jika argumen bertipe float, hasilnya bertipe int.
+Jika argumen bertipe double, hasilnya bertipe long.
+         */
+        System.out.print("Masukan bilangan Desimal = ");
+        float c = in.nextFloat();
+        System.out.println("Hasil dari Math.round = " + Math.round(c));
+        
+    }
+    
+}
