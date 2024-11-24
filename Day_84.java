76f96c0c14151167d814266801e757bdcf12ae6c
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-11-24 22:59:02 +0800

    Evaluasi.java

---
+++ Day_84.java
@@ -0,0 +1,16 @@
+package Coding100Day;
+
+import java.util.Scanner;
+
+public class Day_84 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukan Kalimat = ");
+        String a = in.nextLine();
+        String b = a.toUpperCase();
+        System.out.println("Hasil = " + b);
+    }
+
+}
