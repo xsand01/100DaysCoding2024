ebea5bbd9cf16e931f4c1b64d0d7e47288a5ee06
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-11-25 19:53:16 +0800

    MengisiElemenkeArraySatuDimensiPadaSaatDiInisialisasi.java

--- Day_85.java
+++ Day_85.java
@@ -5,7 +5,7 @@ public class Day_85 {
     //Mengisi elemen ke Array satu dimensi pada saat di inisialisasi 
     public static void main(String[] args) {
         int[] a = {1, 2, 3, 4, 5};
-        for (int i = 0; i <= a.length; i++) {
+        for (int i = 0; i < a.length; i++) {
             System.out.println("index ke - " + i + " = " + a[i]);
         }
     }
