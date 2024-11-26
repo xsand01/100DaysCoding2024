6992450b09b97e3f858afa34ff8d015c8529538a
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-11-26 14:09:05 +0800

    MencetakElemenPadaArraySatuDimensi .java

---
+++ Day_86.java
@@ -0,0 +1,13 @@
+package Coding100Day;
+
+public class Day_86 {
+
+    public static void main(String[] args) {
+        //Mencetak elemen pada Array satu dimensi 
+        String[] a = {"Kurnia Sandy", "18 Tahun", "170cm"}; // INDEKS 0 , INDEKS 1 , INDEKS 2
+        System.out.println("Nama : " + a[0]); // 0 = Kurnia Sandy
+        System.out.println("Umur : " + a[1]); // 1 = 18 Tahun
+        System.out.println("TB   : " + a[2]); // 2 = 170cm
+    }
+
+}
