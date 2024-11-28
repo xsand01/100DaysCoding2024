f4d3063d6881611fcea6293290b97a947e1509cd
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-11-29 00:28:39 +0800

    soal.java

--- Day_88.java
+++ Day_88.java
@@ -4,34 +4,30 @@ import java.util.Scanner;
 
 public class Day_88 {
 
-       /* Buat sebuah program menggunakan Java yang melakukan hal-hal berikut:
- 1. Meminta user untuk menginput jumlah mahasiswa yang ingin didata.
- 2. Meminta user untuk menginput nama mahasiswa satu per satu sebanyak jumlah yang diinputkan sebelumnya.
- 3. Menyimpan nama-nama mahasiswa ke dalam array.
- 4. Menampilkan daftar nama mahasiswa setelah semua data diinputkan. */
-
     public static void main(String[] args) {
+        /*
+        Buat sebuah program menggunakan Java yang melakukan hal-hal berikut:
+1. Meminta user untuk menginput jumlah mahasiswa yang ingin didata.
+2. Meminta user untuk menginput nama mahasiswa satu per satu sebanyak jumlah yang diinputkan sebelumnya.
+3. Menyimpan nama-nama mahasiswa ke dalam array.
+4. Menampilkan daftar nama mahasiswa setelah semua data diinputkan.
+        
+         */
         Scanner in = new Scanner(System.in);
-
-        System.out.print("input jumlah mahasiswa yang ingin didata: ");
-        int jum = in.nextInt();
+        System.out.print("Masukan jumlah mahasiswa yang ingin di input = ");
+        int jumlah = in.nextInt();
         in.nextLine();
-
-        String[] ar = new String[jum];
-        
-        System.out.println("====================================================");
-        
-        for (int i = 0; i < jum; i++) {
-            System.out.print("masukkan nama mahasiswa ke-" + (i + 1) + ": ");
-            ar[i] = in.nextLine();
+        String[] nama = new String[jumlah];
+        for (int i = 0; i < jumlah; i++) {
+            System.out.print("Masukan nama mahasiswa ke " + (i + 1) + " = ");
+            nama[i] = in.nextLine();
         }
-        
-        System.out.println("====================================================");
-        System.out.println("mahasiswa yang sudah di input: ");
-        
-        for (int i = 0; i < jum; i++) {
-            System.out.println("nama mahasiswa ke-"+(i+1)+": "+ar[i]);
+        System.out.println("============================================== ");
+        System.out.println("nama mahasiswa yang di input = ");
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println((i + 1) + "." + nama[i]);
 
         }
+
     }
 }
