b7dfbfd026b404b41291c334eafe3def53d7a2f9
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2024-12-04 13:57:18 +0800

    MembuatPolaSegitiga.java

---
+++ Day_94.java
@@ -0,0 +1,30 @@
+package Coding100Day;
+
+public class Day_94 {
+
+    public static void main(String[] args) {
+        // Membuat pola segitiga
+        
+        int n = 5; // Jumlah baris pola
+
+        // Loop untuk setiap baris
+        for (int i = 1; i <= n; i++) {
+            // Cetak spasi di awal setiap baris
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Cetak bintang di sisi kiri'
+            System.out.print("*");
+
+            // Cetak bintang di tengah (kecuali baris pertama)
+            if (i > 1) {
+                for (int j = 1; j < 2 * (i - 1); j++) {
+                    System.out.print("*");
+                }
+                // Cetak bintang di sisi kanan
+                System.out.print("*");
+            }
+            System.out.println(); // Pindah ke baris berikutnya
+        }
+    }
+}
